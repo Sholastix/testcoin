62b1a80ed6f3dd0ae2b875f58fb2f9fd9bd1212a
Sholastix <drenai6436@gmail.com>
2021-06-10 17:55:25 +0300

    notifications logic added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const request = require('request');
 
 const app = express();
 
@@ -11,6 +12,31 @@ const { Notification } = require('./models/Notification');
 const { User } = require('./models/User');
 
 require('./seed/seeder')();
+
+const data = async () => {
+    const alerts = await Alert.find();
+    alerts.forEach((alert) => {
+        request({
+            // url: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eth',
+            url: `https://api.coingecko.com/api/v3/simple/price?ids=${alert.coinId}&vs_currencies=${alert.currencyId}`,
+            json: true,
+        }, (error, response, body) => {
+            let price = body[alert.coinId][alert.currencyId];
+            if (price > alert.value || price < alert.value) {
+                let message = `The price is ${price > alert.value ? 'GT' : 'LT'} then alert value`;
+                console.log(message);
+                let notification = new Notification({
+                    alertId: alert.id,
+                    message: message,
+                });
+
+                notification.save();
+            }
+        });
+    });
+};
+
+data();
 
 (async () => {
     try {
